79d91bee08d62dcf2fb1ab198984bf4bcf410f2d
Abhay <abhaykumar31099@gmail.com>
2025-02-19 23:44:54 +0530

    add vercal package

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    target: 'esnext', // Ensures modern JavaScript output (ES Modules)
-  },
   assetsInclude: ['**/*.PNG'],
   plugins: [react(),  
     tailwindcss(),
